2199c774d71a49044118d60f3204c2a01d96e3dc
Khant Min Thu <khantminthu@corpit.com.sg>
2023-01-17 17:08:32 +0630

    dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,17 +2,14 @@
   <v-container fluid grid-list-xl>
     <v-layout row wrap>
       <!-- Widgets-->
-      <v-flex d-flex lg3 sm6 xs12>
-        <widget icon="domain" title="1,287,687" :subTitle="$t('widgetHigherYesterday', ['13%'])" :supTitle="$t('widgetTodaysVisit')" color="#00b297"/>
+      <v-flex d-flex lg4 sm6 xs12>
+        <widget icon="money_off" title="$1,287,687"  :supTitle="$t('widgetTodaysTotalAmount')" color="#dc3545"/>
       </v-flex>
-      <v-flex d-flex lg3 sm6 xs12>
-        <widget icon="money_off" title="$141,291" :subTitle="$t('widgetBeforeTax', ['$117,212'])" :supTitle="$t('widgetTodaysSale')" color="#dc3545"/>
+      <v-flex d-flex lg4 sm6 xs12>
+        <widget icon="domain" :title= toDayRegistrationCount :supTitle="$t('widgetTodaysRegistraion')" color="#00b297"/>
       </v-flex>
-      <v-flex d-flex lg3 sm6 xs12>
-        <widget icon="computer" title="33.45%" :subTitle="$t('widgetAverageDuration', ['13%'])"  :supTitle="$t('widgetUniqueVisits')" color="#0866C6"/>
-      </v-flex>
-      <v-flex d-flex lg3 sm6 xs12>
-        <widget icon="watch_later" title="13.00%" :subTitle="$t('widgetAverageTime', ['17.25%'])" :supTitle="$t('widgetBounceRate')" color="#1D2939"/>
+      <v-flex d-flex lg4 sm6 xs12>
+        <widget icon="computer" title="33" :supTitle="$t('widgetTodaysOrder')" color="#0866C6"/>
       </v-flex>
       <!-- Widgets Ends -->
       <!-- Statistics -->
@@ -45,11 +42,40 @@
 </template>
 
 <script>
+import utils from "../utils/utils";
+import api from "../utils/api.js";
 export default {
   data() {
     return {
+      toDayRegistrationCount : 0,
+      todayDate : '',
+      from : '',
+      to: '',
       lorem: `Lorem ipsum dolor sit amet, mel at clita quando.`
     }
+  },
+  async created() {
+    const current = new Date();
+    const date = current.getDate();
+    const month = + current.getMonth()+ 1;
+    const year = + current.getFullYear();
+    this.todayDate = year+ '-' + month + '-' + date
+    // this.from = todayDate + ' ' + '00:00:00';
+    // this.to = todayDate + ' ' + '23:59:59';
+    await this.getToDayRegistration();
+  },
+
+  methods: {
+    async getToDayRegistration() {
+      const resp = await utils.http.get("/user/getToDayRegistration/" + this.todayDate );
+      if (resp) {
+        const data = await resp.json();
+        this.toDayRegistrationCount = data.length;
+        console.log(this.toDayRegistrationCount)
+      } else {
+        console.log("something wrong");
+      }
+    },
   }
 }
 </script>
